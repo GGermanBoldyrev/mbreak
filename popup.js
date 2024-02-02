99c386637d95b492629122e3a9b7b1faf10be32e
GGermanBoldyrev <boldyrev_german@mail.ru>
2024-02-02 14:23:33 +0300

    js grabQuestions function added

--- popup.js
+++ popup.js
@@ -11,6 +11,7 @@ button.addEventListener("click", function () {
 
 // Функция при включении расширения
 function onClick() {
+    // Получаем активную вкладку
     chrome.tabs.query({ active: true }, (tabs) => {
         const tab = tabs[0];
         if (tab) {
@@ -20,13 +21,19 @@ function onClick() {
                 alert("Параметр cmid в строке url не найден. Это точно тест?");
                 return;
             }
-            // Получаем массив вопросов со страницы
-            qText = getQText();
+            // Собираем вопросы в функции grabQuestions
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId: tab.id, allFrames: true },
+                    func: grabQuestions
+                },
+                onResult
+            );
         } else {
             alert("Активных вкладок не найдено.")
-        }
-    })
-}
+        };
+    });
+};
 
 // Получаем код теста
 function getCmid(url) {
@@ -34,13 +41,22 @@ function getCmid(url) {
     // Если нет параметра cmid в строке url
     if (url.search(strToSearch) === -1) {
         return -1;
-    }
-    cmidPos = url.search(strToSearch) + strToSearch.length; // Получаем индекс начала строки и плюсуем str
-    cmid = url.substr(cmidPos, 6)
+    };
+    // Получаем индекс начала строки и плюсуем str
+    cmidPos = url.search(strToSearch) + strToSearch.length;
+    cmid = url.substr(cmidPos, 6);
     return cmid;
 }
 
-// Должна ловить элемент на котоырй нажали и кидать запрос на сервак
-function showAnswers() {
-    return null;
+function grabQuestions() {
+    // TODO - Запросить список div-ов 
+    // и вернуть их список
+    const questions = document.querySelectorAll(".qtext");
+    return Array.from(questions).map(question => question.outerText);
+}
+
+function onResult(frames) {
+    // TODO - Объединить списки URL-ов, полученных из каждого фрейма в один,
+    // затем объединить их в строку, разделенную символом перевода строки
+    // и скопировать в буфер обмена
 }
