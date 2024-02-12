ba18ba17e51eff20c92208ab794a82934fae0090
GGermanBoldyrev <boldyrev_german@mail.ru>
2024-02-12 18:51:22 +0300

    highlightAnswers func is in process

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,13 @@
-// chrome.tabs для доступа к активной вкладке браузера.
-// chrome.scripting для внедрения кода JavaScript на web-страницу активной вкладки 
+// Chrome.tabs для доступа к активной вкладке браузера.
+// Chrome.scripting для внедрения кода JavaScript на web-страницу активной вкладки
 // и для исполнения этого кода в контексте этой страницы.
 
 // TODO: Выводить еще ответы в отдельное окно????
-
 // URL для получения ответов
 const url = "https://moodle-breaker.kmsign.ru/getQuestionResult";
 // Основная кнопка (запуска скрипта)
 const button = document.getElementById("conf-btn");
-// Парамет cmid из URL страницы (переназначаем функцией getCmid(url))
+// Параметр cmid из URL страницы (переназначаем функцией getCmid(url))
 let cmid = 0;
 
 // Onclick по основной кнопке
@@ -16,7 +15,7 @@ button.addEventListener("click", function () {
     onClick();
 });
 
-// Функция которая вызывается на клик по оснвоной кнопке
+// Функция, которая вызывается на клик по основной кнопке
 function onClick() {
     // Получаем активную вкладку
     chrome.tabs.query({active: true}, (tabs) => {
@@ -29,13 +28,9 @@ function onClick() {
                 return;
             }
             // Собираем вопросы в функции grabQuestions
-            chrome.scripting.executeScript(
-                {
-                    target: {tabId: tab.id, allFrames: true},
-                    func: grabQuestions
-                },
-                onResult
-            );
+            chrome.scripting.executeScript({
+                target: {tabId: tab.id, allFrames: true}, func: grabQuestions
+            }, onResult);
         } else {
             alert("Активных вкладок не найдено.")
         }
@@ -73,28 +68,25 @@ async function onResult(frames) {
     // Массив из вопросов
     let questionsArr = frames.map(frame => frame.result).reduce((r1, r2) => r1.concat(r2));
     // Вырезаем ненужные символы и новые строки (если есть)
-    prepareQuestions(questionsArr);
+    // prepareQuestions(questionsArr);
     // Получаем ответы на вопросы
     const answers = await getAnswers(questionsArr);
     console.log(answers)
-    // Запускаем в активной вкладке функцию, которая выделяет
-    // правильные ответы и вопросы на которые ответа нет
+    // Запускаем в активной вкладке функцию, которая показывает юзеру ответы
     await injectHighlightAnswers(answers);
     // Выводим текст, что ответы успешно получены
     success();
 }
 
-// Функуция запроса
+// Функция запроса
 const postData = async (url = '', data = {}) => {
     // POST запрос
     const response = await fetch(url, {
         // Метод
-        method: 'POST',
-        // Заголвоки
+        method: 'POST', // Заголовки
         headers: {
             'Content-Type': 'application/json'
-        },
-        // Данные
+        }, // Данные
         body: JSON.stringify(data)
     });
     // Возвращаем ответ в формате json
@@ -107,24 +99,23 @@ async function getAnswers(questions = []) {
     let answersArr = [];
     // Итерируемся по всем вопросам и отправляем запрос для получения ответа на сервер
     for (const question of questions) {
-        answersArr.push(
-            await postData(url, {"test_id": Number(cmid), "question_text": question})
-                .then(result => {
-                    // Если пустой ответ
-                    if (result["answers"] === null) {
-                        return [question, null];
-                    }
-                    // Если ответ найден то помещаем его в переменную
-                    let answer = result["answers"][0]["text"].trim()
-                    // Если ответ приходит в виде объекта
-                    if (answer.charAt(0) === '{' && answer.charAt(answer.length - 1) === '}') {
-                        // Тогда ответ будет массивом массивов
-                        answer = Object.entries(JSON.parse(answer));
-                        return [question, answer];
-                    }
-                    // Если просто пришел ответ
-                    return [question, answer]
-                }))
+        answersArr.push(await postData(url, {"test_id": Number(cmid), "question_text": question})
+            .then(result => {
+                // Если пустой ответ
+                if (result["answers"] === null) {
+                    return [question, null];
+                }
+                // Если ответ найден, то помещаем его в переменную
+                let answer = result["answers"][0]["text"].trim()
+                // Если ответ приходит в виде объекта
+                if (answer.charAt(0) === '{' && answer.charAt(answer.length - 1) === '}') {
+                    // Тогда ответ будет массивом массивов
+                    answer = Object.entries(JSON.parse(answer));
+                    return [question, answer];
+                }
+                // Если просто пришел ответ
+                return [question, answer]
+            }))
     }
     return answersArr;
 }
@@ -133,34 +124,44 @@ async function getAnswers(questions = []) {
 function injectHighlightAnswers(answers) {
     chrome.tabs.query({active: true}, (tabs) => {
         const tab = tabs[0];
-        chrome.scripting.executeScript(
-            {
-                target: {tabId: tab.id},
-                func: highlightAnswers,
-                args: [answers]
-            }).then(response => console.log(response))
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id}, func: highlightAnswers, args: [answers],
+        }).then(response => console.log(response))
     });
 }
 
 // Функция для подсветки правильных ответов
 function highlightAnswers(answers) {
     // Получаем все вопросы
-    const questions = grabQuestions();
+    const questions = document.querySelectorAll(".qtext");
     // Итерируемся по всем вопросам
-    for (let i = 0; i < questions.length; i++) {
-        // TODO выбираем вопрос // красим его в зеленый или красный
-        // TODO красим нужный ответ в зеленый если он не нулл
-        console.log(i);
-    }
-    // Отписка)
+    questions.forEach(question => {
+        answers.forEach(answer => {
+            // Если ответ совпадает с вопросом
+            if (question.outerText === answer[0]) {
+                // Если ответ не найден (null)
+                if (answer[1] === null) {
+                    // Мы красим вопрос в красный цвет
+                }
+                // Если ответ найден, то красим его в зеленый
+                // TODO: color green
+                // Если ответ - массив, то мы не красим ответ, а выводим его рядом
+                if (Array.isArray(answer[1])) {
+                    // TODO: Вывести массив ответов рядом с вопросом
+                }
+                // TODO: покрасить правильный ответ в зеленый цыет
+            }
+        })
+    })
+    // Прописываем текст в лог активной вкладки
     return "Скрипт по подсветке правильных ответов отработал успешно!";
 }
 
 // Функция для подготовки ответов
-function prepareQuestions(questions) {
+/*function prepareQuestions(questions) {
     // Ищем переносы строки
     const elemToSearch = '\n';
-    // Итерируемся по воросам
+    // Итерируемся по вопросам
     for (let i = 0; i < questions.length; i++) {
         // Ищем последний \n в вопросе
         const newLineIndex = questions[i].lastIndexOf(elemToSearch);
@@ -169,7 +170,7 @@ function prepareQuestions(questions) {
             questions[i] = questions[i].substring(newLineIndex + elemToSearch.length);
         }
     }
-}
+}*/
 
 // div элемент preloader
 let preloaderEl = document.getElementById('preloader');
@@ -181,7 +182,7 @@ function preloaderVisible() {
     preloaderEl.innerText = "Идет загрузка...";
 }
 
-// Фукнция для показа текста ответы получены, вместо preloader
+// Функция для показа текста ответы получены, вместо preloader
 function success() {
     preloaderEl.classList.remove("red-text");
     preloaderEl.classList.add("green-text");
