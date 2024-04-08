d1f6efbb28a6d785578a6f9f57dc7a90d702c4de
GGermanBoldyrev <boldyrev_german@mail.ru>
2024-04-08 09:43:20 +0300

    v 1.0 comments remove

--- popup/popup.js
+++ popup/popup.js
@@ -50,23 +50,18 @@ function getCmid(url) {
 
 // Функция для получения всех вопросов
 function grabQuestions() {
-    // Все вопросы со страницы
     const questions = document.querySelectorAll(".qtext");
-    // Возвращаем массив вопросов
     return Array.from(questions).map(question => question.outerText);
 }
 
 async function onResult(frames) {
     // Запускаем preloader
     preloaderVisible();
-    // Проверяем пустое ли свойство объекта length
     if (!frames || !frames.length) {
         alert("Проблема с получением вопросов");
         return;
     }
-    // Массив из вопросов
     let questionsArr = frames.map(frame => frame.result).reduce((r1, r2) => r1.concat(r2));
-    // Получаем ответы на вопросы
     const answers = await getAnswers(questionsArr);
     // Запускаем в активной вкладке функцию, которая показывает юзеру ответы
     injectHighlightAnswers(answers);
@@ -76,32 +71,26 @@ async function onResult(frames) {
 
 // Функция запроса
 const postData = async (url = '', data = {}) => {
-    // POST запрос
     const response = await fetch(url, {
-        // Метод
-        method: 'POST', // Заголовки
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        }, // Данные
+        },
         body: JSON.stringify(data)
     });
-    // Возвращаем ответ в формате json
     return response.json();
 }
 
 // Функция для получения массива ответов
 async function getAnswers(questions = []) {
-    // Массив ответов
     let answersArr = [];
     // Итерируемся по всем вопросам и отправляем запрос для получения ответа на сервер
     for (const question of questions) {
         answersArr.push(await postData(url, {"test_id": Number(cmid), "question_text": question})
             .then(result => {
-                // Если пустой ответ
                 if (result["answers"] === null) {
                     return [question, null];
                 }
-                // Если ответ найден, то помещаем его в переменную
                 let answer = result["answers"][0]["text"].trim()
                 // Если ответ приходит в виде объекта
                 if (answer.charAt(0) === '{' && answer.charAt(answer.length - 1) === '}') {
@@ -128,28 +117,20 @@ function injectHighlightAnswers(answers) {
 
 // Функция для подсветки правильных ответов
 function highlightAnswers(answers) {
-    // Получаем все вопросы
     const questions = document.querySelectorAll(".qtext");
-    // Итерируемся по всем вопросам
     questions.forEach(question => {
         answers.forEach(answer => {
             // Если ответ совпадает с вопросом
             if (question.outerText === answer[0]) {
-                // Если ответ не найден (null)
                 if (answer[1] === null) {
-                    // Мы красим вопрос в красный цвет
                     question.style.backgroundColor = "rgba(229,27,69,0.45)";
                 } else {
-                    // Если ответ найден, то красим вопрос в зеленый
                     question.style.backgroundColor = "#27cf7b";
-                    // Если ответ - массив, то мы не красим ответ, а выводим его рядом
                     if (Array.isArray(answer[1])) {
-                        // Вывести массив ответов рядом с вопросом
                         console.log(answer)
-                    } else { // Если ответ не массив, то просто красим правильный ответ в зеленый.
+                    } else {
                         // Получаем лист блоков с ответами к конкретному вопросу
                         const answerBlock = question.parentNode.querySelector(".answer");
-                        // Итерируемся блокам с ответами
                         for (const answerEl of answerBlock.children) {
                             // Если блок с ответом содержит подстроку с полученным ответом
                             if (answerEl.outerText.includes(answer[1])) {
@@ -162,7 +143,6 @@ function highlightAnswers(answers) {
             }
         });
     });
-    // Прописываем текст в лог активной вкладки
     return "Скрипт по подсветке правильных ответов отработал успешно!";
 }
 
